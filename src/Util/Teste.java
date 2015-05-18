60c3f735fcd2eccab684f4ccf670363f855dfd2e
Tadeu <Tadeu@192.168.1.11>
2015-05-18 16:16:50 -0300

    fase de testes dos metodos usados na ideia

--- src/Util/Teste.java
+++ src/Util/Teste.java
@@ -16,14 +16,15 @@ import java.util.Date;
  */
 public class Teste {
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
         File arquivo = new File("c:/arquivo.txt");
         DateFormat formatData = new SimpleDateFormat("dd/MM/yyyy HH:mm");
         String data = formatData.format(new Date(arquivo.lastModified()));
+        long tamanho = arquivo.length();  
         System.out.println(data);
+        System.out.println(tamanho);
+        
     }
 
 }
