a38c1ec75a011dd1780dbef089767ae591576b68
Tadeu <Tadeu@192.168.1.11>
2015-05-18 16:00:22 -0300

    fase de testes dos metodos usados na ideia

---
+++ src/Util/Teste.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Util;
+
+import java.io.File;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ *
+ * @author Tadeu
+ */
+public class Teste {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        File arquivo = new File("c:/arquivo.txt");
+        DateFormat formatData = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+        String data = formatData.format(new Date(arquivo.lastModified()));
+        System.out.println(data);
+    }
+
+}
